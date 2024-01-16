dc09abc67cc322cdc303256ad2cb28f8160a8b4b
Roberto <roberto.quadraccia90@gmail.com>
2024-01-16 16:47:33 +0100

    Cleanup functions created

--- src/components/QuestionTimer.jsx
+++ src/components/QuestionTimer.jsx
@@ -4,22 +4,36 @@ const QuestionTimer = ({ timeout, onTimeout }) => {
   const [remainingTime, setRemainingTime] = useState(timeout);
 
   useEffect(() => {
-    setTimeout(() => {
+    console.log("SETTING TIMEOUT");
+    const timer = setTimeout(() => {
       onTimeout();
     }, timeout);
+
+    // We store both setTimeout and setInterval in variables,
+    // we then return clearTimeout/clearInterval as cleanup functions
+    // and pass inside the variable, in order to reset the timer that otherwise,
+    // it would be executed twice due to React.StrictMode
+    return () => {
+      clearTimeout(timer);
+    };
   }, [timeout, onTimeout]);
 
-// Above we should add timeout and onTimeout as dependencies to make sure that this effect function gets re-executed
-// if one of those dependencies changes. It makes sense because of the parent component that
-// should decide that the question timer timeout should change, we also want to reset the timer and set it again.
+  // Above we should add timeout and onTimeout as dependencies to make sure that this effect function gets re-executed
+  // if one of those dependencies changes. It makes sense because of the parent component that
+  // should decide that the question timer timeout should change, we also want to reset the timer and set it again.
 
   useEffect(() => {
-    setInterval(() => {
+    console.log("SETTING INTERVAL");
+    const interval = setInterval(() => {
       setRemainingTime((prevRemainingTime) => prevRemainingTime - 100);
     }, 100);
+
+    return () => {
+      clearInterval(interval);
+    };
   }, []);
 
-  return <progress id="question-time" max={timeout} value={remainingTime}/>;
+  return <progress id="question-time" max={timeout} value={remainingTime} />;
 };
 
 export default QuestionTimer;
